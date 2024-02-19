d94308a1f1a3e9c57ffa25cc13697ebb0d971076
Henry Nguyen <henrynvn09@gmail.com>
2024-02-19 09:59:19 -0800

    add time comparison

---
+++ project1/timeCompare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+hex_file="rv32ui-p-${1}.hex"
+
+# Remove temporary files before running
+rm -f tmp.log
+
+# Clean and compile
+make clean &&
+  DEBUG=3 make &&
+
+# Run the test
+./tinyrv -s "tests/$hex_file" > tmp.log
+
+# Check for errors
+if [ $? -ne 0 ]; then
+  echo "Error: Test failed."
+  exit 1
+fi
+
+# Display differences
+diff tmp.log "debug_traces/${hex_file}.log"
+
